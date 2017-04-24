4e6aa0c75f878ee36ae412d909789283d604b164
Garrett Moon <garrett@pinterest.com>
2017-04-24 15:23:22 -0700

    Closer…

--- examples/ASDKgram/Sample/Utilities.h
+++ examples/ASDKgram/Sample/Utilities.h
@@ -6,7 +6,7 @@
 //
 //  Copyright (c) 2014-present, Facebook, Inc.  All rights reserved.
 //  This source code is licensed under the BSD-style license found in the
-//  LICENSE file in the /ASDK-Licenses directory of this source tree. An additional 
+//  LICENSE file in the /ASDK-Licenses directory of this source tree. An additional
 //  grant of patent rights can be found in the PATENTS file in the same directory.
 //
 //  Modifications to this file made after 4/13/2017 are: Copyright (c) 2017-present,
