bdf921247d5b88efac5800db79e13d182e0770df
Garrett Moon <garrett@pinterest.com>
2017-04-24 15:35:03 -0700

    Closer…

--- examples/ASDKgram/Sample/PhotoFeedBaseController.m
+++ examples/ASDKgram/Sample/PhotoFeedBaseController.m
@@ -14,7 +14,7 @@
 //  Pinterest, Inc.  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
 //  You may obtain a copy of the License at
-//  
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 //  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
--- examples/AnimatedGIF/ASAnimatedImage/AppDelegate.m
+++ examples/AnimatedGIF/ASAnimatedImage/AppDelegate.m
@@ -1,12 +1,10 @@
 //
 //  AppDelegate.m
-//  Sample
-//
-//  Created by Garrett Moon on 3/22/16.
+// Texture
 //
 //  Copyright (c) 2014-present, Facebook, Inc.  All rights reserved.
 //  This source code is licensed under the BSD-style license found in the
-//  LICENSE file in the /ASDK-Licenses directory of this source tree. An additional 
+//  LICENSE file in the /ASDK-Licenses directory of this source tree. An additional
 //  grant of patent rights can be found in the PATENTS file in the same directory.
 //
 //  Modifications to this file made after 4/13/2017 are: Copyright (c) 2017-present,
@@ -15,13 +13,6 @@
 //  You may obtain a copy of the License at
 //  
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-//  FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-//  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
 #import "AppDelegate.h"
