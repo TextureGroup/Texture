71d5b3205d53841ab6172ffb901a03de756bf83c
appleguy <scottgoodson@gmail.com>
2017-05-01 10:51:59 -0700

    [ASNodeController+Beta] Provide an option to allow nodes to own their controllers. (#61)
    * [ASNodeController+Beta] Provide an option to allow nodes to own their controllers.
    
    We should certainly remove this before moving ASNodeController out of Beta. However, I think
    it will require at least ASCollectionNode to be able to retain its top level set of node controllers.
    
    Without this facility built in, it's very difficult for apps supporting both UIKit and ASDK to
    manually manage the controllers and keep them in sync with perfect timing.
    
    * [ASNodeController] Fix one of the #if's.

--- Source/ASNodeController+Beta.h
+++ Source/ASNodeController+Beta.h
@@ -18,10 +18,18 @@
 #import <AsyncDisplayKit/ASDisplayNode.h>
 #import <AsyncDisplayKit/ASDisplayNode+Subclasses.h> // for ASInterfaceState protocol
 
+// Until an ASNodeController can be provided in place of an ASCellNode, some apps may prefer to have
+// nodes keep their controllers alive (and a weak reference from controller to node)
+#define INVERT_NODE_CONTROLLER_OWNERSHIP 0
+
 /* ASNodeController is currently beta and open to change in the future */
 @interface ASNodeController<__covariant DisplayNodeType : ASDisplayNode *> : NSObject <ASInterfaceStateDelegate>
 
+#if INVERT_NODE_CONTROLLER_OWNERSHIP
+@property (nonatomic, weak) DisplayNodeType node;
+#else
 @property (nonatomic, strong) DisplayNodeType node;
+#endif
 
 - (void)loadNode;
 
