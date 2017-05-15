df4c1815317e03369fef1aa5c255ee105d544982
Adlai Holler <adlai@pinterest.com>
2017-05-15 15:52:38 -0700

    Remove unused cell node method

--- Source/Private/ASCellNode+Internal.h
+++ Source/Private/ASCellNode+Internal.h
@@ -24,19 +24,6 @@ NS_ASSUME_NONNULL_BEGIN
 @protocol ASCellNodeInteractionDelegate <NSObject>
 
 /**
- * Notifies the delegate that the specified cell node has done a relayout.
- * The notification is done on main thread.
- *
- * This will not be called due to measurement passes before the node has loaded
- * its view, even if triggered by -setNeedsLayout, as it is assumed these are
- * not relevant to UIKit.  Indeed, these calls can cause consistency issues.
- *
- * @param node A node informing the delegate about the relayout.
- * @param sizeChanged `YES` if the node's `calculatedSize` changed during the relayout, `NO` otherwise.
- */
-- (void)nodeDidRelayout:(ASCellNode *)node sizeChanged:(BOOL)sizeChanged;
-
-/**
  * Notifies the delegate that a specified cell node invalidates it's size what could result into a size change.
  *
  * @param node A node informing the delegate about the relayout.
