c7d8c1348910e43c80b6394608ff88b33c6408e0
Michael Schneider <mischneider1@gmail.com>
2017-05-15 07:18:04 -0700

    Change header

--- Source/ASDisplayNode+Layout.mm
+++ Source/ASDisplayNode+Layout.mm
@@ -1,9 +1,13 @@
 //
-//  ASDisplayNode+ASLayout.m
-//  AsyncDisplayKit
+//  ASDisplayNode+Layout.mm
+//  Texture
 //
-//  Created by Michael Schneider on 5/12/17.
-//  Copyright © 2017 Facebook. All rights reserved.
+//  Copyright (c) 2017-present, Pinterest, Inc.  All rights reserved.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 
 #import <AsyncDisplayKit/ASDisplayNodeExtras.h>
