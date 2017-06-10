bb6e8c7068d6e79874610ceed2103e2eb046e588
Adlai Holler <guy.on.internet@icloud.com>
2017-06-10 18:29:53 -0500

    Update the licenses #important

--- Tests/ASCALayerTests.m
+++ Tests/ASCALayerTests.m
@@ -1,9 +1,13 @@
 //
-//  ASCALayerTests.m
+//  NSInvocation+ASTestHelpers.h
 //  Texture
 //
-//  Created by Adlai Holler on 9/2/16.
-//  Copyright © 2016 Facebook. All rights reserved.
+//  Copyright (c) 2017-present, Pinterest, Inc.  All rights reserved.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 
 #import <XCTest/XCTest.h>
