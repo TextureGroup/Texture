4d6a22d11992f2e60f964b8c6f22db162d9ade4d
Adlai Holler <guy.on.internet@icloud.com>
2017-06-10 19:40:24 -0500

    Fix the license header

--- Source/Base/ASAssert.m
+++ Source/Base/ASAssert.m
@@ -1,9 +1,13 @@
 //
 //  ASAssert.m
-//  AsyncDisplayKit
+//  Texture
 //
-//  Created by Adlai Holler on 6/10/17.
-//  Copyright © 2017 Facebook. All rights reserved.
+//  Copyright (c) 2017-present, Pinterest, Inc.  All rights reserved.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 
 #import <AsyncDisplayKit/ASAssert.h>
