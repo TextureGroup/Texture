9ad3fa68f8052548d71992a796399856ab75fdb7
Adlai Holler <guy.on.internet@icloud.com>
2017-06-29 22:05:02 -0700

    Minor cleanup work

--- Source/Private/ASDispatch.h
+++ Source/Private/ASDispatch.h
@@ -16,6 +16,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <stdatomic.h>
 
 /**
  * Like dispatch_apply, but you can set the thread count. 0 means 2*active CPUs.
@@ -23,16 +24,16 @@
  * Note: The actual number of threads may be lower than threadCount, if libdispatch
  * decides the system can't handle it. In reality this rarely happens.
  */
-static void ASDispatchApply(size_t iterationCount, dispatch_queue_t queue, NSUInteger threadCount, void(^work)(size_t i)) {
+static void ASDispatchApply(size_t iterationCount, dispatch_queue_t queue, NSUInteger threadCount, NS_NOESCAPE void(^work)(size_t i)) {
   if (threadCount == 0) {
-    threadCount = [NSProcessInfo processInfo].activeProcessorCount * 2;
+    threadCount = NSProcessInfo.processInfo.activeProcessorCount * 2;
   }
   dispatch_group_t group = dispatch_group_create();
-  __block size_t trueI = 0;
+  __block atomic_size_t counter = ATOMIC_VAR_INIT(0);
   for (NSUInteger t = 0; t < threadCount; t++) {
     dispatch_group_async(group, queue, ^{
       size_t i;
-      while ((i = __sync_fetch_and_add(&trueI, 1)) < iterationCount) {
+      while ((i = atomic_fetch_add(&counter, 1)) < iterationCount) {
         work(i);
       }
     });
