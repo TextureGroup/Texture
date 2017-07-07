94c8e5577b158f4c4c5986eaacecff9231a30a4b
Adlai Holler <adlai@pinterest.com>
2017-07-06 18:03:47 -0700

    Add our license header to debugbreak.

--- Tests/Common/debugbreak.h
+++ Tests/Common/debugbreak.h
@@ -1,3 +1,15 @@
+//
+//  debugbreak.h
+//  Texture
+//
+//  Copyright (c) 2017-present, Pinterest, Inc.  All rights reserved.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+
 /* Copyright (c) 2011-2015, Scott Tsai
  * 
  * All rights reserved.
