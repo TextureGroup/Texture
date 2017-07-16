929d118c962dcf595386daf5c91ce30e1f8e577d
Max Wang <max.wang.cs@gmail.com>
2017-07-16 16:59:28 -0700

    fix SIMULATE_WEB_RESPONSE not imported #449

--- examples/ASCollectionView/Sample/ViewController.m
+++ examples/ASCollectionView/Sample/ViewController.m
@@ -16,7 +16,7 @@
 //
 
 #import "ViewController.h"
-
+#import "AppDelegate.h"
 #import <AsyncDisplayKit/AsyncDisplayKit.h>
 #import "SupplementaryNode.h"
 #import "ItemNode.h"
@@ -70,8 +70,8 @@
     {
       NSLog(@"ViewController is not nil");
       strongSelf->_data = [[NSArray alloc] init];
-      [strongSelf->_collectionView performBatchUpdates:^{
-        [strongSelf->_collectionView insertSections:[[NSIndexSet alloc] initWithIndexesInRange:NSMakeRange(0, 100)]];
+      [strongSelf->_collectionNode performBatchUpdates:^{
+        [strongSelf->_collectionNode insertSections:[[NSIndexSet alloc] initWithIndexesInRange:NSMakeRange(0, 100)]];
       } completion:nil];
       NSLog(@"ViewController finished updating collectionView");
     }
@@ -81,7 +81,7 @@
   };
   
   dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), mockWebService);
-  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(4 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
     [self.navigationController popViewControllerAnimated:YES];
   });
 #endif
