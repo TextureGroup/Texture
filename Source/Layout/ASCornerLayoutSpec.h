de4c56639521ddbe387f1d26dbe208401eeef33a
huangkun <jack-huang-developer@foxmail.com>
2017-10-23 23:32:39 +0800

    Correct license.

--- Source/Layout/ASCornerLayoutSpec.h
+++ Source/Layout/ASCornerLayoutSpec.h
@@ -2,8 +2,12 @@
 //  ASCornerLayoutSpec.h
 //  Texture
 //
-//  Created by huangkun on 2017/10/22.
-//  Copyright © 2017年 AsyncDisplayKit. All rights reserved.
+//  Copyright (c) 2017-present, Pinterest, Inc.  All rights reserved.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 
 #import <AsyncDisplayKit/ASLayoutSpec.h>
