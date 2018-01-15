61dade6bda20ff1ad0003fbd79159046705fcb55
Adlai Holler <adlai@pinterest.com>
2018-01-15 15:13:05 -0800

    Raise deployment target to iOS 9 (#743)
    https://github.com/TextureGroup/Texture/pull/743
    
    Manually merged since I forgot to retarget that diff onto master before merge

--- Source/ASCellNode.mm
+++ Source/ASCellNode.mm
@@ -87,7 +87,7 @@
     if ([_viewController isKindOfClass:[ASViewController class]]) {
       ASViewController *asViewController = (ASViewController *)_viewController;
       _viewControllerNode = asViewController.node;
-      [_viewController view];
+      [_viewController loadViewIfNeeded];
     } else {
       // Careful to avoid retain cycle
       UIViewController *viewController = _viewController;
