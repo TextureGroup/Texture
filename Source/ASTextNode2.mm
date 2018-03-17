0b101135ba9033f7dd48889f25b2b99087034928
Michael Schneider <maicki@users.noreply.github.com>
2018-03-16 20:08:19 -0700

    Handle nil backgroundColor in ASTextNode2 #trivial (#841)
    * Handle nil backgroundColor
    
    * Small improvement

--- Source/ASTextNode2.mm
+++ Source/ASTextNode2.mm
@@ -360,10 +360,10 @@ static NSArray *DefaultLinkAttributeNames = @[ NSLinkAttributeName ];
   [self prepareAttributedString:mutableText];
   
   return @{
-           @"container": copiedContainer,
-           @"text": mutableText,
-           @"bgColor": self.backgroundColor
-           };
+    @"container": copiedContainer,
+    @"text": mutableText,
+    @"bgColor": self.backgroundColor ?: [NSNull null]
+  };
 }
 
 /**
@@ -467,9 +467,13 @@ static NSArray *DefaultLinkAttributeNames = @[ NSLinkAttributeName ];
   }
   
   // Fill background color.
+  if (bgColor == (id)[NSNull null]) {
+    bgColor = nil;
+  }
+
   // They may have already drawn into this context in the pre-context block
   // so unfortunately we have to use the normal blend mode, not copy.
-  if (CGColorGetAlpha(bgColor.CGColor) > 0) {
+  if (bgColor && CGColorGetAlpha(bgColor.CGColor) > 0) {
     [bgColor setFill];
     UIRectFillUsingBlendMode(bounds, kCGBlendModeNormal);
   }
