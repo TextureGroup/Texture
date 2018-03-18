22e9111b247b27d04451f3480c5b8aa136d7ed3a
Adlai Holler <adlai@pinterest.com>
2018-03-17 21:34:09 -0700

    Update changelog and do the thing with the license headers

--- Source/Private/TextExperiment/String/ASTextAttribute.m
+++ Source/Private/TextExperiment/String/ASTextAttribute.m
@@ -1,12 +1,18 @@
 //
 //  ASTextAttribute.m
-//  Modified from YYText <https://github.com/ibireme/YYText>
+//  Texture
 //
-//  Created by ibireme on 14/10/26.
-//  Copyright (c) 2015 ibireme.
+//  Copyright (c) 2014-present, Facebook, Inc.  All rights reserved.
+//  This source code is licensed under the BSD-style license found in the
+//  LICENSE file in the /ASDK-Licenses directory of this source tree. An additional
+//  grant of patent rights can be found in the PATENTS file in the same directory.
 //
-//  This source code is licensed under the MIT-style license found in the
-//  LICENSE file in the root directory of this source tree.
+//  Modifications to this file made after 4/13/2017 are: Copyright (c) through the present,
+//  Pinterest, Inc.  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 
 #import "ASTextAttribute.h"
