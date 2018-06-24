fe6e4167af6344328aa684b674cf93bdad8593cd
Adlai Holler <adlai@pinterest.com>
2018-06-24 13:56:28 -0700

    License headers

--- Tests/ASCollectionsTests.m
+++ Tests/ASCollectionsTests.m
@@ -1,9 +1,13 @@
 //
 //  ASCollectionsTests.m
-//  AsyncDisplayKitTests
+//  Texture
 //
-//  Created by Adlai on 6/22/18.
-//  Copyright © 2018 Pinterest. All rights reserved.
+//  Copyright (c) 2018-present, Pinterest, Inc.  All rights reserved.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 
 #import <XCTest/XCTest.h>
