a015a7c7e6ca3dbae1711c908f0acd1fd9e3290c
Max Wang <max.wang.cs@gmail.com>
2018-08-21 22:12:16 -0700

    Fix collection cell editing bug for iOS 9 & 10

--- Source/ASCollectionView.mm
+++ Source/ASCollectionView.mm
@@ -147,6 +147,12 @@ static NSString * const kReuseIdentifier = @"_ASCollectionReuseIdentifier";
    * (0 sections) we always check at least once after each update (initial reload is the first update.)
    */
   BOOL _hasEverCheckedForBatchFetchingDueToUpdate;
+
+  /**
+   * Set during beginInteractiveMovementForItemAtIndexPath and UIGestureRecognizerStateEnded
+   * (or UIGestureRecognizerStateFailed, UIGestureRecognizerStateCancelled.
+   */
+  BOOL _editing;
   
   /**
    * Counter used to keep track of nested batch updates.
@@ -1021,9 +1027,28 @@ static NSString * const kReuseIdentifier = @"_ASCollectionReuseIdentifier";
 - (void)moveItemAtIndexPath:(NSIndexPath *)indexPath toIndexPath:(NSIndexPath *)newIndexPath
 {
   ASDisplayNodeAssertMainThread();
-  [self performBatchUpdates:^{
-    [_changeSet moveItemAtIndexPath:indexPath toIndexPath:newIndexPath animationOptions:kASCollectionViewAnimationNone];
-  } completion:nil];
+  if (!_editing) {
+    [self performBatchUpdates:^{
+      [_changeSet moveItemAtIndexPath:indexPath toIndexPath:newIndexPath animationOptions:kASCollectionViewAnimationNone];
+    } completion:nil];
+  } else {
+    [super moveItemAtIndexPath:indexPath toIndexPath:newIndexPath];
+  }
+}
+
+- (BOOL)beginInteractiveMovementForItemAtIndexPath:(NSIndexPath *)indexPath {
+  _editing = YES;
+  return [super beginInteractiveMovementForItemAtIndexPath:indexPath];
+}
+
+- (void)endInteractiveMovement {
+  _editing = NO;
+  return [super endInteractiveMovement];
+}
+
+- (void)cancelInteractiveMovement {
+  _editing = NO;
+  [super cancelInteractiveMovement];
 }
 
 #pragma mark -
