351de39cd6aba734b38a59a427c511c98600eedd
Jason Yu <jasy@outlook.com>
2018-09-29 16:49:32 +0800

    fix: incorrect backing image scale after precompositing rounded corners

--- Source/Private/ASDisplayNode+AsyncDisplay.mm
+++ Source/Private/ASDisplayNode+AsyncDisplay.mm
@@ -323,7 +323,7 @@
       bounds.size.height *= contentsScale;
       CGFloat white = 0.0f, alpha = 0.0f;
       [backgroundColor getWhite:&white alpha:&alpha];
-      ASGraphicsBeginImageContextWithOptions(bounds.size, (alpha == 1.0f), contentsScale);
+      ASGraphicsBeginImageContextWithOptions(bounds.size, (alpha == 1.0f), (*image).scale);
       [*image drawInRect:bounds];
     } else {
       bounds = CGContextGetClipBoundingBox(context);
