f07b8af6fa4b71f2fd1528e0a276201e8075e164
Huy Nguyen <huy@pinterest.com>
2018-10-01 17:18:08 -0700

    Fix experiment settings

--- Source/ASExperimentalFeatures.h
+++ Source/ASExperimentalFeatures.h
@@ -21,10 +21,9 @@ typedef NS_OPTIONS(NSUInteger, ASExperimentalFeatures) {
   ASExperimentalUnfairLock = 1 << 3,                        // exp_unfair_lock
   ASExperimentalLayerDefaults = 1 << 4,                     // exp_infer_layer_defaults
   ASExperimentalNetworkImageQueue = 1 << 5,                 // exp_network_image_queue
-  ASExperimentalDeallocQueue = 1 << 6,                      // exp_dealloc_queue_v2
-  ASExperimentalCollectionTeardown = 1 << 7,                // exp_collection_teardown
-  ASExperimentalFramesetterCache = 1 << 8,                  // exp_framesetter_cache
-  ASExperimentalClearDataDuringDeallocation = 1 << 9,       // exp_clear_data_during_deallocation
+  ASExperimentalCollectionTeardown = 1 << 6,                // exp_collection_teardown
+  ASExperimentalFramesetterCache = 1 << 7,                  // exp_framesetter_cache
+  ASExperimentalClearDataDuringDeallocation = 1 << 8,       // exp_clear_data_during_deallocation
   ASExperimentalFeatureAll = 0xFFFFFFFF
 };
 
