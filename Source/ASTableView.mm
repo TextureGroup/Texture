982447d7ff9b5eeff271813caceabae4514ffe7d
Michael Schneider <mischneider1@gmail.com>
2018-10-26 15:31:19 -0700

    Cleanup in ASTableView datasource/delegate change

--- Source/ASTableView.mm
+++ Source/ASTableView.mm
@@ -436,6 +436,7 @@ static NSString * const kCellReuseIdentifier = @"_ASTableViewCell";
   
   _dataController.validationErrorSource = asyncDataSource;
   super.dataSource = (id<UITableViewDataSource>)_proxyDataSource;
+  [self _asyncDelegateOrDataSourceDidChange];
 }
 
 - (id<ASTableDelegate>)asyncDelegate
@@ -506,6 +507,16 @@ static NSString * const kCellReuseIdentifier = @"_ASTableViewCell";
   }
   
   super.delegate = (id<UITableViewDelegate>)_proxyDelegate;
+  [self _asyncDelegateOrDataSourceDidChange];
+}
+
+- (void)_asyncDelegateOrDataSourceDidChange
+{
+  ASDisplayNodeAssertMainThread();
+  
+  if (_asyncDataSource == nil && _asyncDelegate == nil && _isDeallocating && ASActivateExperimentalFeature(ASExperimentalClearDataDuringDeallocation)) {
+    [_dataController clearData];
+  }
 }
 
 - (void)proxyTargetHasDeallocated:(ASDelegateProxy *)proxy
