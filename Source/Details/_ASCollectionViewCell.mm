5dcb9d703291de0ae08201261edea77285a7496d
Deloge, Xavier <xdeloge@lapresse.ca>
2019-03-05 09:00:43 -0500

    Removed isNodeLoaded, finally it's useless in this case In these methods, we are on the mainThread, we can create the view if the view is not created.

--- Source/Details/_ASCollectionViewCell.mm
+++ Source/Details/_ASCollectionViewCell.mm
@@ -110,22 +110,14 @@
     return nil;
   }
 
-  if ([self.node isNodeLoaded] && self.node.view) {
-    CGPoint pointOnNode = [self.node.view convertPoint:point fromView:self];
-    return [self.node hitTest:pointOnNode withEvent:event];
-  } else {
-    return [self.node hitTest:point withEvent:event];
-  }
+  CGPoint pointOnNode = [self.node.view convertPoint:point fromView:self];
+  return [self.node hitTest:pointOnNode withEvent:event];
 }
 
 - (BOOL)pointInside:(CGPoint)point withEvent:(nullable UIEvent *)event
 {
-  if ([self.node isNodeLoaded] && self.node.view) {
-    CGPoint pointOnNode = [self.node.view convertPoint:point fromView:self];
-    return [self.node pointInside:pointOnNode withEvent:event];
-  } else {
-    return [self.node pointInside:point withEvent:event];
-  }
+  CGPoint pointOnNode = [self.node.view convertPoint:point fromView:self];
+  return [self.node pointInside:pointOnNode withEvent:event];
 }
 
 @end
