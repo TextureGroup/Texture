c47cecaeac5166c97f441bbdefa3b0dd788ca85a
Michael Schneider <mischneider1@gmail.com>
2019-03-08 07:26:11 -0800

    Update header

--- Source/ASButtonNode+Private.h
+++ Source/ASButtonNode+Private.h
@@ -1,9 +1,9 @@
 //
 //  ASButtonNode+Private.h
-//  AsyncDisplayKit
+//  Texture
 //
-//  Created by Michael Schneider on 3/7/19.
-//  Copyright © 2019 Pinterest. All rights reserved.
+//  Copyright (c) Pinterest, Inc.  All rights reserved.
+//  Licensed under Apache 2.0: http://www.apache.org/licenses/LICENSE-2.0
 //
 
 #import <AsyncDisplayKit/ASButtonNode.h>
