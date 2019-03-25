27cc0f27175f66623aa93037085b131e1e0e5694
Michael Zuccarino <mikezuccarino@gmail.com>
2019-03-25 14:11:04 -0700

    Experiment with disabling ASViewController background dealloc (#1420)

--- Tests/ASConfigurationTests.mm
+++ Tests/ASConfigurationTests.mm
@@ -31,7 +31,8 @@ static ASExperimentalFeatures features[] = {
   ASExperimentalDispatchApply,
   ASExperimentalImageDownloaderPriority,
   ASExperimentalTextDrawing,
-  ASExperimentalFixRangeController
+  ASExperimentalFixRangeController,
+  ASExperimentalOOMBackgroundDeallocDisable
 };
 
 @interface ASConfigurationTests : ASTestCase <ASConfigurationDelegate>
@@ -57,7 +58,8 @@ static ASExperimentalFeatures features[] = {
     @"exp_dispatch_apply",
     @"exp_image_downloader_priority",
     @"exp_text_drawing",
-    @"exp_fix_range_controller"
+    @"exp_fix_range_controller",
+    @"exp_oom_bg_dealloc_disable"
   ];
 }
 
