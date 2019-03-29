d4384b36c97a4feca3c9a3dfaffc37ffd9194e4e
Adlai Holler <adlai@google.com>
2019-03-29 07:25:59 -0700

    Add a new "global drawing" experiment to use UIGraphicsRenderer everywhere, when available

--- Tests/ASConfigurationTests.mm
+++ Tests/ASConfigurationTests.mm
@@ -34,6 +34,7 @@ static ASExperimentalFeatures features[] = {
   ASExperimentalOOMBackgroundDeallocDisable,
   ASExperimentalTransactionOperationRetainCycle,
   ASExperimentalRemoveTextKitInitialisingLock,
+  ASExperimentalDrawingGlobal
 };
 
 @interface ASConfigurationTests : ASTestCase <ASConfigurationDelegate>
@@ -61,7 +62,8 @@ static ASExperimentalFeatures features[] = {
     @"exp_fix_range_controller",
     @"exp_oom_bg_dealloc_disable",
     @"exp_transaction_operation_retain_cycle",
-    @"exp_remove_textkit_initialising_lock"
+    @"exp_remove_textkit_initialising_lock",
+    @"exp_drawing_global"
   ];
 }
 
