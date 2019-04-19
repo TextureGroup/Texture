29aedccde2e447a7b5720423da23951c16519ab2
Huy Nguyen <huy@pinterest.com>
2019-04-19 12:41:27 -0700

    Add AS_ENABLE_THREAD_ASSERTIONS build flag

--- Source/Details/ASThread.h
+++ Source/Details/ASThread.h
@@ -98,8 +98,17 @@ ASDISPLAYNODE_INLINE void _ASUnlockScopeCleanup(id<NSLocking> __strong *lockPtr)
 #include <thread>
 
 // These macros are here for legacy reasons. We may get rid of them later.
-#define ASAssertLocked(m)
-#define ASAssertUnlocked(m)
+#ifndef AS_ENABLE_THREAD_ASSERTIONS
+  #define AS_ENABLE_THREAD_ASSERTIONS 0
+#endif
+
+#if AS_ENABLE_THREAD_ASSERTIONS
+  #define ASAssertLocked(m) m.AssertHeld()
+  #define ASAssertUnlocked(m) m.AssertNotHeld()
+#else
+  #define ASAssertLocked(m)
+  #define ASAssertUnlocked(m)
+#endif
 
 namespace AS {
   
