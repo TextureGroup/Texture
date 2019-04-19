cc8fb840fa77b45b32d57fe1ee8ead076631ae01
Adlai Holler <adlai@google.com>
2019-04-19 14:57:52 -0700

    Fix

--- Source/Details/_ASDisplayView.mm
+++ Source/Details/_ASDisplayView.mm
@@ -207,7 +207,7 @@ static _ASDisplayViewMethodOverrides GetASDisplayViewMethodOverrides(Class c)
     if (needsSupernodeUpdate) {
       // -removeFromSupernode is called by -addSubnode:, if it is needed.
       // FIXME: Needs rethinking if automaticallyManagesSubnodes=YES
-      [newSuperview.asyncdisplaykit_node _addSubnode:node];
+      [newSuperview.asyncdisplaykit_node addSubnode:node];
     }
   }
 }
