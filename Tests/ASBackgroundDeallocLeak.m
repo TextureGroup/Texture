b04d540647485a2a50370c65af19b886890f37f8
Greg Bolsinga <gbolsinga@pinterest.com>
2019-05-02 10:59:18 -0700

    move stuff around. now it is clear that things run -later- on various run loops to clean things up. run the run loop outside of the autoreleasepool scope to see this. it needs to be run for 0.2 instead of 0.1 secs on my setup

--- Tests/ASBackgroundDeallocLeak.m
+++ Tests/ASBackgroundDeallocLeak.m
@@ -19,57 +19,82 @@
 
 @implementation ASBackgroundDeallocLeak
 
+- (void)testNode
+{
+  __weak ASViewController *weakViewController = nil;
+  __weak ASDisplayNode *weakNode = nil;
+
+  @autoreleasepool {
+    ASDisplayNode *node = [[ASDisplayNode alloc] init];
+    weakNode = node;
+
+    ASViewController *viewController = [[ASViewController alloc] initWithNode:node];
+    weakViewController = viewController;
+
+    viewController = nil;
+  }
+
+  // Let things clean up.
+  [[NSRunLoop mainRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.2]];
+
+  XCTAssertNil(weakViewController);
+  XCTAssertNil(weakNode);
+}
+
 - (void)testViewBacked_Background
 {
   __weak ASViewController *weakViewController = nil;
+  __weak ASDisplayNode *weakNode = nil;
   __weak id displayLayer = nil;
 
   @autoreleasepool {
-    ASViewController *viewController = [[ASViewController alloc] initWithNode:[[ASDisplayNode alloc] init]];
+    ASDisplayNode *node = [[ASDisplayNode alloc] init];
+    weakNode = node;
+
+    ASViewController *viewController = [[ASViewController alloc] initWithNode:node];
     weakViewController = viewController;
     displayLayer = viewController.node.view;
+
     viewController = nil;
-
-    // intends to semaphore mainthread until an arbitrary amount dispatch queue creates and block dispatches
-    // loosely confirm that all background queues have been flushed
-    //    [ASViewControllerTests haltMainUntilBackgroundFlushes];
-
-    [[ASDeallocQueue sharedDeallocationQueue] drain];
-
-    [[NSRunLoop mainRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:2]];
   }
 
+  // Let things clean up.
+  [[NSRunLoop mainRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.2]];
+
   XCTAssertNil(weakViewController);
+  XCTAssertNil(weakNode);
   XCTAssertNil(displayLayer);
 }
 
 - (void)testLayerBacked_Background
 {
   __weak ASViewController *weakViewController = nil;
+  __weak ASDisplayNode *weakNode = nil;
   __weak id displayLayer = nil;
 
   @autoreleasepool {
-    ASViewController *viewController = [[ASViewController alloc] initWithNode:[[ASDisplayNode alloc] init]];
+    ASDisplayNode *node = [[ASDisplayNode alloc] init];
+    weakNode = node;
+
+    ASViewController *viewController = [[ASViewController alloc] initWithNode:node];
     weakViewController = viewController;
     displayLayer = viewController.node.layer;
+
     viewController = nil;
-
-    // intends to semaphore mainthread until an arbitrary amount dispatch queue creates and block dispatches
-    // loosely confirm that all background queues have been flushed
-    //    [ASViewControllerTests haltMainUntilBackgroundFlushes];
-
-    [[ASDeallocQueue sharedDeallocationQueue] drain];
-
-    [[NSRunLoop mainRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:2]];
   }
 
+  // Let things clean up.
+  [[NSRunLoop mainRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.2]];
+
   XCTAssertNil(weakViewController);
+  XCTAssertNil(weakNode);
   XCTAssertNil(displayLayer);
 }
 
 - (void)testViewBacked_Default
 {
   __weak ASViewController *weakViewController = nil;
+  __weak ASDisplayNode *weakNode = nil;
   __weak id displayLayer = nil;
 
   ASConfiguration *config = [[ASConfiguration alloc] initWithDictionary:nil];
@@ -77,27 +102,28 @@
   [ASConfigurationManager test_resetWithConfiguration:config];
 
   @autoreleasepool {
-    ASViewController *viewController = [[ASViewController alloc] initWithNode:[[ASDisplayNode alloc] init]];
+    ASDisplayNode *node = [[ASDisplayNode alloc] init];
+    weakNode = node;
+
+    ASViewController *viewController = [[ASViewController alloc] initWithNode:node];
     weakViewController = viewController;
     displayLayer = viewController.node.view;
+
     viewController = nil;
-
-    // intends to semaphore mainthread until an arbitrary amount dispatch queue creates and block dispatches
-    // loosely confirm that all background queues have been flushed
-    //    [ASViewControllerTests haltMainUntilBackgroundFlushes];
-
-    [[ASDeallocQueue sharedDeallocationQueue] drain];
-
-    [[NSRunLoop mainRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:2]];
   }
 
+  // Let things clean up.
+  [[NSRunLoop mainRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.2]];
+
   XCTAssertNil(weakViewController);
+  XCTAssertNil(weakNode);
   XCTAssertNil(displayLayer);
 }
 
 - (void)testLayerBacked_Default
 {
   __weak ASViewController *weakViewController = nil;
+  __weak ASDisplayNode *weakNode = nil;
   __weak id displayLayer = nil;
 
   ASConfiguration *config = [[ASConfiguration alloc] initWithDictionary:nil];
@@ -105,54 +131,22 @@
   [ASConfigurationManager test_resetWithConfiguration:config];
 
   @autoreleasepool {
-    ASViewController *viewController = [[ASViewController alloc] initWithNode:[[ASDisplayNode alloc] init]];
+    ASDisplayNode *node = [[ASDisplayNode alloc] init];
+    weakNode = node;
+
+    ASViewController *viewController = [[ASViewController alloc] initWithNode:node];
     weakViewController = viewController;
     displayLayer = viewController.node.layer;
+
     viewController = nil;
-
-    // intends to semaphore mainthread until an arbitrary amount dispatch queue creates and block dispatches
-    // loosely confirm that all background queues have been flushed
-    //    [ASViewControllerTests haltMainUntilBackgroundFlushes];
-
-    [[ASDeallocQueue sharedDeallocationQueue] drain];
-
-    [[NSRunLoop mainRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:2]];
   }
 
+  // Let things clean up.
+  [[NSRunLoop mainRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.2]];
+
   XCTAssertNil(weakViewController);
+  XCTAssertNil(weakNode);
   XCTAssertNil(displayLayer);
 }
-
-
-//+ (void)haltMainUntilBackgroundFlushes
-//{
-//  assert([NSThread isMainThread]);
-//  dispatch_semaphore_t sema = dispatch_semaphore_create(9001);
-//  int max_count = 10;
-//  __block BOOL GCDFlushed = NO;
-//  for (int i=0; i < max_count; i++) {
-//    dispatch_async(dispatch_get_global_queue(QOS_CLASS_DEFAULT, 0), ^{
-//      usleep(10000);
-//      if (i == 1000) {
-//        GCDFlushed = YES;
-//        dispatch_semaphore_signal(sema); // this doesnt work yet
-//      }
-//    });
-//  }
-//  dispatch_semaphore_wait(sema, dispatch_time(DISPATCH_TIME_NOW, NSEC_PER_SEC * 3));
-//  return GCDFlushed;
-//}
-
-+ (UIImage *)imageOfSize:(CGSize)size filledWithColor:(UIColor *)color {
-  UIGraphicsBeginImageContextWithOptions(size, NO, 0.0);
-  CGContextRef context = UIGraphicsGetCurrentContext();
-  CGRect rect = (CGRect){CGPointZero, size};
-  CGContextFillRect(context, rect);
-  UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-  UIGraphicsEndImageContext();
-  return image;
-}
-
-
 
 @end
