e1859a1029e082551d6d5980da5d88a9491e0f3a
Huy Nguyen <huy@pinterest.com>
2019-05-21 22:23:24 -0700

    Ship ASExperimentalImageDownloaderPriority (#1480)

--- Source/ASExperimentalFeatures.h
+++ Source/ASExperimentalFeatures.h
@@ -28,12 +28,11 @@ typedef NS_OPTIONS(NSUInteger, ASExperimentalFeatures) {
   ASExperimentalDidEnterPreloadSkipASMLayout = 1 << 7,      // exp_did_enter_preload_skip_asm_layout
   ASExperimentalDisableAccessibilityCache = 1 << 8,         // exp_disable_a11y_cache
   ASExperimentalDispatchApply = 1 << 9,                     // exp_dispatch_apply
-  ASExperimentalImageDownloaderPriority = 1 << 10,          // exp_image_downloader_priority
-  ASExperimentalTextDrawing = 1 << 11,                      // exp_text_drawing
-  ASExperimentalOOMBackgroundDeallocDisable = 1 << 12,      // exp_oom_bg_dealloc_disable
-  ASExperimentalTransactionOperationRetainCycle = 1 << 13,  // exp_transaction_operation_retain_cycle
-  ASExperimentalRemoveTextKitInitialisingLock = 1 << 14,    // exp_remove_textkit_initialising_lock
-  ASExperimentalDrawingGlobal = 1 << 15,                    // exp_drawing_global
+  ASExperimentalTextDrawing = 1 << 10,                      // exp_text_drawing
+  ASExperimentalOOMBackgroundDeallocDisable = 1 << 11,      // exp_oom_bg_dealloc_disable
+  ASExperimentalTransactionOperationRetainCycle = 1 << 12,  // exp_transaction_operation_retain_cycle
+  ASExperimentalRemoveTextKitInitialisingLock = 1 << 13,    // exp_remove_textkit_initialising_lock
+  ASExperimentalDrawingGlobal = 1 << 14,                    // exp_drawing_global
   ASExperimentalFeatureAll = 0xFFFFFFFF
 };
 
