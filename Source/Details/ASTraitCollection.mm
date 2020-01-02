005e2d5aa9b760c123240b85c86425c029319cc6
Andrew Yates <contact@andydev.co.uk>
2020-01-02 18:37:42 +0000

    First pass at supporting new traits (#1568)

--- Source/Details/ASTraitCollection.mm
+++ Source/Details/ASTraitCollection.mm
@@ -41,6 +41,11 @@ ASPrimitiveTraitCollection ASPrimitiveTraitCollectionMakeDefault() {
   if (AS_AVAILABLE_IOS_TVOS(12, 10)) {
     tc.userInterfaceStyle = UIUserInterfaceStyleUnspecified;
   }
+  if(AS_AVAILABLE_IOS(13)){
+    tc.userInterfaceLevel = UIUserInterfaceLevelUnspecified;
+    tc.accessibilityContrast = UIAccessibilityContrastUnspecified;
+    tc.legibilityWeight = UILegibilityWeightUnspecified;
+  }
   return tc;
 }
 
@@ -60,6 +65,11 @@ ASPrimitiveTraitCollection ASPrimitiveTraitCollectionFromUITraitCollection(UITra
   }
   if (AS_AVAILABLE_IOS_TVOS(12, 10)) {
     environmentTraitCollection.userInterfaceStyle = traitCollection.userInterfaceStyle;
+  }
+  if(AS_AVAILABLE_IOS(13)){
+    environmentTraitCollection.userInterfaceLevel = traitCollection.userInterfaceLevel;
+    environmentTraitCollection.accessibilityContrast = traitCollection.accessibilityContrast;
+    environmentTraitCollection.legibilityWeight = traitCollection.legibilityWeight;
   }
   return environmentTraitCollection;
 }
@@ -169,6 +179,47 @@ ASDISPLAYNODE_INLINE NSString *AS_NSStringFromUIUserInterfaceStyle(UIUserInterfa
   }
 }
 
+// Named so as not to conflict with a hidden Apple function, in case compiler decides not to inline
+API_AVAILABLE(ios(13))
+ASDISPLAYNODE_INLINE NSString *AS_NSStringFromUITraitEnvironmentUserInterfaceLevel(UIUserInterfaceLevel userInterfaceLevel) {
+  switch (userInterfaceLevel) {
+    case UIUserInterfaceLevelBase:
+      return @"Base";
+    case UIUserInterfaceLevelElevated:
+      return @"Elevated";
+    default:
+      return @"Unspecified";
+  }
+}
+
+// Named so as not to conflict with a hidden Apple function, in case compiler decides not to inline
+API_AVAILABLE(ios(13))
+ASDISPLAYNODE_INLINE NSString *AS_NSStringFromUITraitEnvironmentAccessibilityContrast(UIAccessibilityContrast accessibilityContrast) {
+  switch (accessibilityContrast) {
+    case UIAccessibilityContrastNormal:
+      return @"Normal";
+    case UIAccessibilityContrastHigh:
+      return @"High";
+    default:
+      return @"Unspecified";
+  }
+}
+
+// Named so as not to conflict with a hidden Apple function, in case compiler decides not to inline
+API_AVAILABLE(ios(13))
+ASDISPLAYNODE_INLINE NSString *AS_NSStringFromUITraitEnvironmentLegibilityWeight(UILegibilityWeight legibilityWeight) {
+  switch (legibilityWeight) {
+    case UILegibilityWeightRegular:
+      return @"Regular";
+    case UILegibilityWeightBold:
+      return @"Bold";
+    default:
+      return @"Unspecified";
+  }
+}
+
+
+
 NSString *NSStringFromASPrimitiveTraitCollection(ASPrimitiveTraitCollection traits) {
   NSMutableArray<NSDictionary *> *props = [NSMutableArray array];
   [props addObject:@{ @"verticalSizeClass": AS_NSStringFromUIUserInterfaceSizeClass(traits.verticalSizeClass) }];
@@ -183,6 +234,11 @@ NSString *NSStringFromASPrimitiveTraitCollection(ASPrimitiveTraitCollection trai
     [props addObject:@{ @"layoutDirection": AS_NSStringFromUITraitEnvironmentLayoutDirection(traits.layoutDirection) }];
     [props addObject:@{ @"preferredContentSizeCategory": traits.preferredContentSizeCategory }];
     [props addObject:@{ @"displayGamut": AS_NSStringFromUIDisplayGamut(traits.displayGamut) }];
+  }
+  if (AS_AVAILABLE_IOS(13)){
+    [props addObject:@{ @"userInterfaceLevel": AS_NSStringFromUITraitEnvironmentUserInterfaceLevel(traits.userInterfaceLevel) }];
+    [props addObject:@{ @"accessibilityContrast": AS_NSStringFromUITraitEnvironmentAccessibilityContrast(traits.accessibilityContrast) }];
+    [props addObject:@{ @"legibilityWeight": AS_NSStringFromUITraitEnvironmentLegibilityWeight(traits.legibilityWeight) }];
   }
   [props addObject:@{ @"containerSize": NSStringFromCGSize(traits.containerSize) }];
   return ASObjectDescriptionMakeWithoutObject(props);
@@ -244,6 +300,19 @@ NSString *NSStringFromASPrimitiveTraitCollection(ASPrimitiveTraitCollection trai
 {
   return _prim.preferredContentSizeCategory;
 }
+- (UIUserInterfaceLevel)userInterfaceLevel
+{
+  return _prim.userInterfaceLevel;
+}
+- (UIAccessibilityContrast)accessibilityContrast
+{
+  return _prim.accessibilityContrast;
+}
+- (UILegibilityWeight)legibilityWeight
+{
+  return _prim.legibilityWeight;
+}
+
 - (NSUInteger)hash {
   return ASHashBytes(&_prim, sizeof(ASPrimitiveTraitCollection));
 }
