a6f9ceec710722cd61be7ad570f4f9439463b29d
Greg Bolsinga <gbolsinga@pinterest.com>
2020-05-26 20:56:28 -0700

    Ship ASExperimentalUnfairLock experiment (#1841)
    - Followup to #1742
    - At Pinterest this shipped with D516974 in late 02/2020
    - As discussed in #858 this is iOS10 or later, so the runtime `gMutex_unfair` check is still necessary for Texture.

--- Source/ASExperimentalFeatures.h
+++ Source/ASExperimentalFeatures.h
@@ -20,17 +20,16 @@ typedef NS_OPTIONS(NSUInteger, ASExperimentalFeatures) {
   // will be used in all cases
   ASExperimentalTextNode = 1 << 0,                                          // exp_text_node
   ASExperimentalInterfaceStateCoalescing = 1 << 1,                          // exp_interface_state_coalesce
-  ASExperimentalUnfairLock = 1 << 2,                                        // exp_unfair_lock
-  ASExperimentalLayerDefaults = 1 << 3,                                     // exp_infer_layer_defaults
-  ASExperimentalCollectionTeardown = 1 << 4,                                // exp_collection_teardown
-  ASExperimentalFramesetterCache = 1 << 5,                                  // exp_framesetter_cache
-  ASExperimentalSkipClearData = 1 << 6,                                     // exp_skip_clear_data
-  ASExperimentalDidEnterPreloadSkipASMLayout = 1 << 7,                      // exp_did_enter_preload_skip_asm_layout
-  ASExperimentalDispatchApply = 1 << 8,                                     // exp_dispatch_apply
-  ASExperimentalDrawingGlobal = 1 << 9,                                     // exp_drawing_global
-  ASExperimentalOptimizeDataControllerPipeline = 1 << 10,                   // exp_optimize_data_controller_pipeline
-  ASExperimentalTraitCollectionDidChangeWithPreviousCollection = 1 << 11,   // exp_trait_collection_did_change_with_previous_collection
-  ASExperimentalDoNotCacheAccessibilityElements = 1 << 12,                  // exp_do_not_cache_accessibility_elements
+  ASExperimentalLayerDefaults = 1 << 2,                                     // exp_infer_layer_defaults
+  ASExperimentalCollectionTeardown = 1 << 3,                                // exp_collection_teardown
+  ASExperimentalFramesetterCache = 1 << 4,                                  // exp_framesetter_cache
+  ASExperimentalSkipClearData = 1 << 5,                                     // exp_skip_clear_data
+  ASExperimentalDidEnterPreloadSkipASMLayout = 1 << 6,                      // exp_did_enter_preload_skip_asm_layout
+  ASExperimentalDispatchApply = 1 << 7,                                     // exp_dispatch_apply
+  ASExperimentalDrawingGlobal = 1 << 8,                                     // exp_drawing_global
+  ASExperimentalOptimizeDataControllerPipeline = 1 << 9,                    // exp_optimize_data_controller_pipeline
+  ASExperimentalTraitCollectionDidChangeWithPreviousCollection = 1 << 10,   // exp_trait_collection_did_change_with_previous_collection
+  ASExperimentalDoNotCacheAccessibilityElements = 1 << 11 ,                  // exp_do_not_cache_accessibility_elements
   ASExperimentalFeatureAll = 0xFFFFFFFF
 };
 
