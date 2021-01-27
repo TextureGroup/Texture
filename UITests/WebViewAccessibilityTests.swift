403af66313397f6b433908f770a0a65a65f8ba76
Zev Eisenberg <zev.eisenberg@nytimes.com>
2021-01-27 11:17:43 -0500

    Add UI test to make sure web view stays accessible.

--- UITests/WebViewAccessibilityTests.swift
+++ UITests/WebViewAccessibilityTests.swift
@@ -19,25 +19,12 @@ class AsyncDisplayKitUITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
+    func testWebViewAccessibility() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+      XCTAssert(app.staticTexts["Texture is Awesome!"].exists)
+      XCTAssert(app.staticTexts["Especially when web views inside nodes are accessible."].exists)
+  }
 }
