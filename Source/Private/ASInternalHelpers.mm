053688bfb2746775f691a6841497e9e91ee06081
David Ha <h2s1880@gmail.com>
2021-12-04 02:32:05 +0900

    Xcode 13 ASLoadFrameworkInitializer dead lock fix on running unit test (#2032)
    * replace destructor for after main execute
    
    * separate ASLoadFrameworkInitializer between constructor and destructor
    
    * AS_EXTERN -> ASDK_EXTERN
    
    * initialSetNeedsDisplayCount should be called once on iOS 15
    
    * Loaded node of contentsScale must be 2.0 on layer backed
    
    * initialSetNeedsDisplayCount must called once

--- Source/Private/ASInternalHelpers.mm
+++ Source/Private/ASInternalHelpers.mm
@@ -61,7 +61,19 @@ void _ASInitializeSignpostObservers(void)
 }
 #endif  // AS_SIGNPOST_ENABLE
 
-void ASInitializeFrameworkMainThread(void)
+void ASInitializeFrameworkMainThreadOnConstructor(void)
+{
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    ASDisplayNodeCAssertMainThread();
+    ASNotifyInitialized();
+#if AS_SIGNPOST_ENABLE
+    _ASInitializeSignpostObservers();
+#endif
+  });
+}
+
+void ASInitializeFrameworkMainThreadOnDestructor(void)
 {
   static dispatch_once_t onceToken;
   dispatch_once(&onceToken, ^{
@@ -74,10 +86,6 @@ void ASInitializeFrameworkMainThread(void)
       allowsGroupOpacityFromUIKitOrNil = @(layer.allowsGroupOpacity);
       allowsEdgeAntialiasingFromUIKitOrNil = @(layer.allowsEdgeAntialiasing);
     }
-    ASNotifyInitialized();
-#if AS_SIGNPOST_ENABLE
-    _ASInitializeSignpostObservers();
-#endif
   });
 }
 
