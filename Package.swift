c0f37dc790a4b91b1550ce3c734f1d61a5b9ed01
Roman <85091414+Fireroman@users.noreply.github.com>
2022-11-24 14:51:41 +0300

    Create Package.swift
    Package.Swift file for Swift Package Manager.

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.0
+ import PackageDescription
+
+ let package = Package(
+      name: "Texture",
+      platforms: [
+          .iOS(.v9)
+      ],
+      products: [
+          .library(name: "Texture", targets: ["Texture"])
+      ],
+      targets: [
+         .target(
+                name: "Texture",
+                path: "Source"
+         )
+      ]
+  )
