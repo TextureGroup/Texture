86b55b95c15043cbf0b2a5c53a770e721abe7899
Patryk Średziński <sredzinski.patryk@gmail.com>
2024-12-05 16:37:03 +0100

    Package json

--- Package.swift
+++ Package.swift
@@ -1,49 +1,38 @@
-// swift-tools-version:5.7
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
-let headersSearchPath: [CSetting] = [.headerSearchPath("."),
-                                     .headerSearchPath("Base"),
-                                     .headerSearchPath("Debug"),
-                                     .headerSearchPath("Details"),
-                                     .headerSearchPath("Details/Transactions"),
-                                     .headerSearchPath("Layout"),
-                                     .headerSearchPath("Private"),
-                                     .headerSearchPath("Private/Layout"),
-                                     .headerSearchPath("TextExperiment/Component"),
-                                     .headerSearchPath("TextExperiment/String"),
-                                     .headerSearchPath("TextExperiment/Utility"),
-                                     .headerSearchPath("TextKit"),
-                                     .headerSearchPath("tvOS"),]
-
-let sharedDefines: [CSetting] = [
-                                // Disable "old" textnode by default for SPM
-                                .define("AS_ENABLE_TEXTNODE", to: "0"),
-    
-                                // PINRemoteImage always available for Texture
-                                .define("AS_PIN_REMOTE_IMAGE", to: "1"),
-                                
-                                // always disabled
-                                .define("IG_LIST_COLLECTION_VIEW", to: "0"),]
+let headersSearchPath: [CSetting] = [
+    .headerSearchPath("."),
+    .headerSearchPath("Base"),
+    .headerSearchPath("Debug"),
+    .headerSearchPath("Details"),
+    .headerSearchPath("Details/Transactions"),
+    .headerSearchPath("Layout"),
+    .headerSearchPath("Private"),
+    .headerSearchPath("Private/Layout"),
+    .headerSearchPath("TextExperiment/Component"),
+    .headerSearchPath("TextExperiment/String"),
+    .headerSearchPath("TextExperiment/Utility"),
+    .headerSearchPath("TextKit"),
+    .headerSearchPath("tvOS"),
+]
 
 let package = Package(
-    name: "AsyncDisplayKit",
-    platforms: [
-        .iOS(.v8)
-    ],
+    name: "Texture",
     products: [
         .library(
-            name: "AsyncDisplayKit",
-            targets: ["AsyncDisplayKit"]
-        )
-    ],
-    dependencies: [
-        .package(url: "https://github.com/pinterest/PINRemoteImage.git", .branch("master")),
+            name: "Texture",
+            targets: ["Texture"]
+        ),
     ],
     targets: [
         .target(
-            name: "AsyncDisplayKit",
-            dependencies: ["PINRemoteImage"],
-            path: "Source"
+            name: "Texture",
+            path: "Source",
+            publicHeadersPath: ".",
+            cSettings: headersSearchPath
         )
     ]
 )
