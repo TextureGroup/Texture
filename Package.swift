1a3ec29aabf722a15a0acb58791b42d1f3b0b9fe
Patryk Średziński <sredzinskipatryk@gmail.com>
2024-12-05 15:43:50 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,81 +1,77 @@
-// swift-tools-version: 5.7
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
+
+let headersSearchPath: [CSetting] = [.headerSearchPath("."),
+                                     .headerSearchPath("Base"),
+                                     .headerSearchPath("Debug"),
+                                     .headerSearchPath("Details"),
+                                     .headerSearchPath("Details/Transactions"),
+                                     .headerSearchPath("Layout"),
+                                     .headerSearchPath("Private"),
+                                     .headerSearchPath("Private/Layout"),
+                                     .headerSearchPath("TextExperiment/Component"),
+                                     .headerSearchPath("TextExperiment/String"),
+                                     .headerSearchPath("TextExperiment/Utility"),
+                                     .headerSearchPath("TextKit"),
+                                     .headerSearchPath("tvOS"),]
+
+let sharedDefines: [CSetting] = [
+                                // Disable "old" textnode by default for SPM
+                                .define("AS_ENABLE_TEXTNODE", to: "0"),
+    
+                                // PINRemoteImage always available for Texture
+                                .define("AS_PIN_REMOTE_IMAGE", to: "1"),
+                                
+                                // always disabled
+                                .define("IG_LIST_COLLECTION_VIEW", to: "0"),]
+
+func IGListKit(enabled: Bool) -> [CSetting] {
+    let state: String = enabled ? "1" : "0"
+    return [
+        .define("AS_IG_LIST_KIT", to: state),
+        .define("AS_IG_LIST_DIFF_KIT", to: state),
+    ]
+}
+
 
 let package = Package(
     name: "Texture",
     platforms: [
-        .iOS(.v14),
-        .tvOS(.v14)
-    ],
+             .macOS(.v10_15),
+             .iOS(.v10),
+             .tvOS(.v10)
+         ],
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Texture",
-            targets: ["Texture"]
-        )
+            name: "AsyncDisplayKit",
+            type: .static,
+            targets: ["AsyncDisplayKit"]),
+        .library(
+            name: "AsyncDisplayKitIGListKit",
+            type: .static,
+            targets: ["AsyncDisplayKitIGListKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pinterest/PINRemoteImage.git", from: "3.0.0"),
-        .package(url: "https://github.com/Instagram/IGListKit.git", .upToNextMajor(from: "4.0.0")),
-        .package(url: "https://github.com/facebook/yoga.git", .exact("2.0.0"))
+        .package(url: "https://github.com/pinterest/PINRemoteImage.git", .branch("master")),
+        .package(url: "https://github.com/3a4oT/IGListKit", .branch("spmNumber10")),
     ],
     targets: [
         .target(
-            name: "Texture",
-            dependencies: [
-                .target(name: "Core"),
-                .product(name: "PINRemoteImage", package: "PINRemoteImage"),
-                .product(name: "IGListKit", package: "IGListKit"),
-                .product(name: "Yoga", package: "yoga")
-            ],
-            path: "Source",
-            exclude: [
-                "AsyncDisplayKit+Tips",
-                "Examples",
-                "Tests"
-            ],
-            publicHeadersPath: "AsyncDisplayKit",
-            cSettings: [
-                .headerSearchPath("Source"),
-                .headerSearchPath("Source/Details"),
-                .headerSearchPath("Source/Layout"),
-                .headerSearchPath("Source/Base"),
-                .headerSearchPath("Source/Debug"),
-                .headerSearchPath("Source/TextKit"),
-                .headerSearchPath("Source/TextExperiment"),
-                .define("USE_TEXTURE")
-            ]
+            name: "AsyncDisplayKit",
+            dependencies: ["PINRemoteImage"],
+            path: "spm/Sources/AsyncDisplayKit",
+            cSettings: headersSearchPath + sharedDefines + IGListKit(enabled: false)
         ),
         .target(
-            name: "Core",
-            dependencies: [],
-            path: "Source/Core"
+            name: "AsyncDisplayKitIGListKit",
+            dependencies: ["IGListKit", "PINRemoteImage"],
+            path: "spm/Sources/AsyncDisplayKitIGListKit/AsyncDisplayKit",
+            cSettings: headersSearchPath + sharedDefines + IGListKit(enabled: true)
         ),
-        .testTarget(
-            name: "TextureTests",
-            dependencies: ["Texture"],
-            path: "Tests"
-        ),
-        .target(
-            name: "PINRemoteImage",
-            dependencies: [
-                .product(name: "PINRemoteImage", package: "PINRemoteImage")
-            ],
-            path: "Source/PINRemoteImage"
-        ),
-        .target(
-            name: "IGListKit",
-            dependencies: [
-                .product(name: "IGListKit", package: "IGListKit")
-            ],
-            path: "Source/IGListKit"
-        ),
-        .target(
-            name: "Yoga",
-            dependencies: [
-                .product(name: "Yoga", package: "yoga")
-            ],
-            path: "Source/Yoga"
-        )
     ],
-    swiftLanguageVersions: [.v5]
+    cLanguageStandard: .c11,
+    cxxLanguageStandard: .cxx11
 )
